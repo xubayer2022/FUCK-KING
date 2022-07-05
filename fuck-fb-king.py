2f86d7f58b005d2cc1483078ad0d310b9077cbe7
CYBER-TALHA <97680832+xubayer2022@users.noreply.github.com>
2022-07-06 02:08:07 +0600

    Create fuck-fb-king.py

---
+++ fuck-fb-king.py
@@ -0,0 +1,14 @@
+import os, platform, time
+try:
+    import requests
+except:
+    os.system('pip install requests')
+    os.system('git pull')
+    import requests
+bit = platform.architecture()[0]
+if bit == '64bit':
+    print("\n\x1b[1;92m Congratulations ! Your Device FUCKED this Tools")
+    print(' FUCK FB-KING TALHA ZUBAYER😍 ')
+    os.system('xdg-open https://www.facebook.com/groups/348077704169065/');time.sleep(3)
+    from V4 import Main
+    Main()
